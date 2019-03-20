6abafffabac4f9a8304e8654a9685ba7368a0361
Dean Roddey <droddey@charmedquark.com>
2019-03-20 13:11:04 -0400

    Get rid of out of date old tests that aren't really tests. We can start bringing some of them back in updated form as more advanced samples

--- Source/AllProjects/Tests/TestLogSrv2/run.cmd
+++
@@ -1,4 +0,0 @@
-start CIDNameSrv /LocalLog=%CID_RESDIR%/Logs/Log.Txt;UTF-16;CQCLgMtx
-start CIDLogSrv /LocalLog=%CID_RESDIR%/Logs/Log.Txt;UTF-16;CQCLgMtx /OutPath=%CID_RESDIR%/Logs
-REM devenv /debugexe \CID_Dev\DevResult\CIDLogSrv.exe /LocalLog=%CID_RESDIR%/Logs/Log.Txt;UTF-16;CQCLgMtx /OutPath=%CID_RESDIR%/Logs
-
