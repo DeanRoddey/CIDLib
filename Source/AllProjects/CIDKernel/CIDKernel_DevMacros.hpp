388e05d09a96d283fd9950ba6ec775d26b5a31d6
Dean Roddey <droddey@charmedquark.com>
2019-04-09 19:08:44 -0400

    A small fix for reflected gradient text sometimes showing some incorrect bits of the reflection shadow at the very bottom.

--- Source/AllProjects/CIDKernel/CIDKernel_DevMacros.hpp
+++ Source/AllProjects/CIDKernel/CIDKernel_DevMacros.hpp
@@ -177,6 +177,10 @@ public : \
 // ---------------------------------------------------------------------------
 //  For putting message pragma reminders into code
 // ---------------------------------------------------------------------------
+#if CID_DEBUG_ON
 #define MakeLINEStr(M, L) M(L)
 #define $LineStr MakeLINEStr(CIDLib_Stringize, __LINE__)
 #define CodeReminder __FILE__ "(" $LineStr ") : "
+#else
+#define CodeReminder
+#endif
