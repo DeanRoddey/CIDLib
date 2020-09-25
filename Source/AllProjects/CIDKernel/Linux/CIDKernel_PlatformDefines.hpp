e77c6edb7964d4b1ce18f182544139d0d56e81f0
Declan Hoare <declanhoare@declanhoare.id.au>
2020-09-25 18:56:04 +1000

    Namespace the argc and argv externs
    This matches how they are defined and prevents a link error.

--- Source/AllProjects/CIDKernel/Linux/CIDKernel_PlatformDefines.hpp
+++ Source/AllProjects/CIDKernel/Linux/CIDKernel_PlatformDefines.hpp
@@ -73,6 +73,15 @@
 
 #define CIDLIB_POPPACK  pack(pop)
 
+// ---------------------------------------------------------------------------
+//  Declare the global variables for argc and argv
+// ---------------------------------------------------------------------------
+namespace CIDKernel_SystemInfo_Linux
+{
+    extern int argc;
+    extern char** argv;
+}
+
 
 // ---------------------------------------------------------------------------
 //  Define our version of the magic main module macro. We need to grab argc
@@ -83,10 +92,8 @@
 #define CIDLib_MainModule(thrCtor) \
 int main(int argc, char** argv) \
 { \
-    extern int CIDKernel_SystemInfo_Linux_argc; \
-    extern char** CIDKernel_SystemInfo_Linux_argv; \
-    CIDKernel_SystemInfo_Linux_argc = argc; \
-    CIDKernel_SystemInfo_Linux_argv = argv; \
+    CIDKernel_SystemInfo_Linux::argc = argc; \
+    CIDKernel_SystemInfo_Linux::argv = argv; \
     TThread* pthrToStart = new thrCtor; \
     CIDLib_MakePrimary(pthrToStart); \
     CIDLib_Init(); \
